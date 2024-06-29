d5add584690bcc2f555dffeb5584bcbf3024df37
Noobowski <p.a.kons@yandex.ru>
2024-06-29 12:37:31 +0300

    создал html сайт

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,20 +1,28 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import web.service.CarService;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class CarController {
+    private final CarService carService;
+
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
     @GetMapping(value = "/car")
     public String myCarMeth(ModelMap model) {
         List<String> message = new ArrayList<>();
         message.add("Car");
-        model.addAttribute("Messages", message);
-
+        model.addAttribute("cars", carService.getCars());
 
         return "car";
     }
