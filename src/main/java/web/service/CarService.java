e553b217472168d049404fd8aa6046c5496f90fc
Noobowski <p.a.kons@yandex.ru>
2024-06-29 22:58:47 +0300

    убрал public из конструктора, незначительные изменения

--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -5,6 +5,6 @@ import web.model.Car;
 import java.util.List;
 
 public interface CarService {
-    public List<Car> getCars(int count);
+    List<Car> getCars(int count);
 
 }
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -9,11 +9,13 @@ import java.util.stream.Collectors;
 @Service
 public class CarServiceImpl implements CarService {
     public List<Car> getCars(int count) {
-        List<Car> result = List.of(new Car(1L, "BMW", "Black"),
+        List<Car> result = List.of(
+                new Car(1L, "BMW", "Black"),
                 new Car(2L, "Audi", "White"),
                 new Car(3L, "Mercedes", "White"),
                 new Car(4L, "Rolls Royce", "Red"),
-                new Car(5L, "Lada", "White"));
+                new Car(5L, "Lada", "White")
+        );
         return result.stream()
                 .limit(count)
                 .collect(Collectors.toList());
